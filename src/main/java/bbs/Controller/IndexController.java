206aa78166fb9591e19f00d0fd9f25baa42dc7ce
StanHustler <cf260469080@sina.com>
2022-10-03 09:16:24 +0800

    Import BootStrap

--- src/main/java/bbs/Controller/IndexController.java
+++ src/main/java/bbs/Controller/IndexController.java
@@ -1,13 +1,13 @@
 package bbs.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
     @GetMapping({"", "/", "/index", "/index.html"})
-    public String indexPage() {
+    public String index() {
         return "index";
     }
 }
