9ffde7be8ab65692d213928ffc15d4190cad9afe
Stan Hustler <cf260469080@sina.com>
2022-10-06 18:37:54 +0800

    Use session to keep login status

--- src/main/java/bbs/controller/IndexController.java
+++ src/main/java/bbs/controller/IndexController.java
@@ -1,13 +1,38 @@
 package bbs.controller;
 
+import bbs.mapper.UserMapper;
+import bbs.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping({"", "/", "/index", "/index.html"})
-    public String index() {
+    public String index(HttpServletRequest request) {
+        // find the cookie which named token
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        System.out.println("found cookie, setting session"); //sout
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
+
         return "index";
     }
 }
