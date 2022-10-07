5b1d70b10d5b58b819e92005008b2078f333b520
Stan Hustler <cf260469080@sina.com>
2022-10-07 12:04:36 +0800

    new table post, alter table user

--- mysql.sql
+++ mysql.sql
@@ -6,6 +6,21 @@ create table user
     name         varchar(60)  null,
     token        char(36)     null,
     gmt_create   bigint       null,
-    gmt_modified bigint       null
+    gmt_modified bigint       null,
+    bio          varchar(256) null comment 'biography'
 );
 
+create table post
+(
+    id            int auto_increment
+        primary key,
+    title         varchar(50)   null,
+    description   text          null,
+    gmt_create    bigint        null,
+    gmt_modified  bigint        null,
+    creator       int           null,
+    comment_count int default 0 null,
+    view_count    int default 0 null,
+    like_count    int default 0 null,
+    tag           varchar(256)  null
+);
