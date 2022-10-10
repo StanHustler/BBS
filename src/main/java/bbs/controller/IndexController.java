b06e48d950d308401e9625eb5df349a4eb084e12
Stan Hustler <cf260469080@sina.com>
2022-10-10 22:27:28 +0800

    [New] profileController Add interceptor to keep away guests.

--- src/main/java/bbs/controller/IndexController.java
+++ src/main/java/bbs/controller/IndexController.java
@@ -1,8 +1,6 @@
 package bbs.controller;
 
 import bbs.dto.PaginationDTO;
-import bbs.mapper.UserMapper;
-import bbs.model.User;
 import bbs.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,37 +8,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class IndexController {
 
     @Autowired
-    private UserMapper userMapper;
-    @Autowired
     private PostService postService;
 
     @GetMapping({"", "/", "/index", "/index.html"})
-    public String index(HttpServletRequest request,
-                        Model model,
+    public String index(Model model,
                         @RequestParam(name = "page", defaultValue = "1") Integer page,
                         @RequestParam(name = "size", defaultValue = "5") Integer size) {
-        // find the cookie which named token
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if (user != null) {
-                        System.out.println("found cookie, setting session"); //sout
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
 
         // get posts and show them in index page
         PaginationDTO pagination = postService.list(page,size);
--- src/main/java/bbs/controller/ProfileController.java
+++ src/main/java/bbs/controller/ProfileController.java
@@ -1,21 +1,31 @@
 package bbs.controller;
 
+import bbs.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ProfileController {
 
     @GetMapping("/profile/{action}")
     public String profile(@PathVariable(name = "action")String action,
-                          Model model){
+                          Model model,
+                          HttpServletRequest request){
 
+        User user =(User) request.getSession().getAttribute("user");
+        if (user == null) return "redirect:/";
+
+        model.addAttribute("section",action);
         if (action.equals("posts")){
-            model.addAttribute("section","posts");
             model.addAttribute("sectionName","我的发表");
+        } else if (action.equals("replies")) {
+            model.addAttribute("sectionName","最新回复");
         }
+
         return "profile";
     }
 }
